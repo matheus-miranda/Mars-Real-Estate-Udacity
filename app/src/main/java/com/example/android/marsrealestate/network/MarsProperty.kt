81d9571a5f9f195196d863caf8db5a7af243fb8e
matheus-miranda <matheus.sm86@gmail.com>
2021-09-11 17:12:46 -0300

    Create MarsProperty DTO for API response

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,4 +17,18 @@
 
 package com.example.android.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    @Json(name = "price")
+    val price: Double,
+
+    @Json(name = "id")
+    val id: String,
+
+    @Json(name = "type")
+    val type: String,
+
+    @Json(name = "img_src")
+    val imgSrcUrl: String
+)
