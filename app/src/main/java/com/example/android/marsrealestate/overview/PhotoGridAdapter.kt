98af3bd1b420ed66b72dc065ba1da4287888d478
matheus-miranda <matheus.sm86@gmail.com>
2021-09-12 00:44:26 -0300

    Create PhotoGridAdapter

--- app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -16,3 +16,44 @@
  */
 
 package com.example.android.marsrealestate.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsProperty
+
+class PhotoGridAdapter :
+    ListAdapter<MarsProperty, PhotoGridAdapter.ItemViewHolder>(DiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ItemViewHolder(GridViewItemBinding.inflate(inflater))
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val marsProperty = getItem(position)
+        holder.bind(marsProperty)
+    }
+
+    inner class ItemViewHolder(private val binding: GridViewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(marsProperty: MarsProperty) {
+            binding.property = marsProperty
+            binding.executePendingBindings()
+        }
+    }
+}
+
+class DiffCallBack : DiffUtil.ItemCallback<MarsProperty>() {
+    override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+        return oldItem == newItem
+    }
+}
