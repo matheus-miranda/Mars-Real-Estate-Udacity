0d5bfa9ddfed0024af81eb3f6271bc9ab5d60e31
matheus-miranda <matheus.sm86@gmail.com>
2021-09-11 21:14:35 -0300

    Apply placeholder and error image

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,6 +21,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 // Convert image URL to a URI with the http scheme
 @BindingAdapter("imageUrl")
@@ -29,6 +30,9 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = it.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
+            .apply(RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_connection_error))
             .into(imgView)
     }
 }
